e4321b99a6dc8e7dc422286a26fa620d234baab6
Thiago Zampieri <thiago.zampieri@gmail.com>
2023-11-20 14:57:00 -0300

    update module and clean code

--- app/code/local/Looship/Admin/Helper/Data.php
+++ app/code/local/Looship/Admin/Helper/Data.php
@@ -1,8 +1,107 @@
 <?php
 class Looship_Admin_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public function getLooid()
+    public function getHost()
     {
-        return Mage::getSingleton('core/session')->getLooid();
+        return 'https://api.looship.com.br';
+    }
+
+    public function getLooId()
+    {
+        return Mage::getSingleton('core/session')->getLooId();
+    }
+
+    public function getLooMedia()
+    {
+        $media = Mage::getSingleton('core/session')->getLooMedia();
+        if (!$media) {
+            return array(
+                "reference"    => Mage::app()->getRequest()->getServer('HTTP_REFERER'),
+                "utm_source"   => "none",
+                "utm_campaign" => "none",
+                "utm_medium"   => "site"
+            );
+        }
+        return $media;
+    }
+
+    public function getOrderItems()
+    {
+        $items = [];
+        $cart = Mage::getModel('checkout/cart');
+        $cart_items = $cart->getQuote()->getAllItems();
+        foreach ($cart_items as $item) 
+        {
+            $productId = $item->getProductId();
+            $product = Mage::getModel('catalog/product')->load($productId);
+            $items[] = array(
+                "sku"      => $product->getSku(),
+                "name"     => $product->getName(),
+                "amount"   => $item->getQty(),
+                "subtotal" => $item->getPrice(),
+                "taxonomy" => $product->getAttributeText('taxonomy'),
+                "weight"   => (float) $product->getWeight(),
+            );
+        }
+
+        return $items;
+    }
+
+    public function getShippingRates($shippingRates)
+    {
+        $freights = [];
+        if (isset($shippingRates) && is_array($shippingRates))
+        {
+            foreach ($shippingRates as $rate) 
+            {
+                if ($rate) 
+                {
+                    $freights[] = array(
+                        "label" => $rate->getCode(),
+                        "time"  => (int) $rate->getTimeInTransit(),
+                        "value" => $rate->getPrice(),
+                    );
+                }
+            }
+        }
+        return $freights;
+    }
+
+    public function postRequest($url, $postData)
+    {
+        try 
+        {
+            $token = Mage::getStoreConfig('shipping/looship_admin/token');
+            if ($token !== '')
+            {
+                $jsonData = json_encode($postData);
+                Mage::log($jsonData, null, 'looship_debug.log');
+                $headers = array(
+                    'Content-Type: application/json',
+                    'Content-Length: ' . strlen($jsonData),
+                    'Authorization: Bearer ' . $token,
+                );
+
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+                $response = curl_exec($ch);
+                $curl_error = curl_error($ch);
+                if ($curl_error) throw new Exception($curl_error);
+                curl_close($ch);
+
+                Mage::log($response, null, 'looship_debug.log');
+                $responseData = json_decode($response, true);
+                $newData = $responseData['data'];
+                return $newData;
+            }
+            return [];
+        } catch (Exception $e) {
+            Mage::log(json_encode($e->getMessage()), null, 'looship_request.log');
+        }
     }
 }
+?>
--- app/code/local/Looship/Admin/Model/Observer.php
+++ app/code/local/Looship/Admin/Model/Observer.php
@@ -4,134 +4,102 @@ class Looship_Admin_Model_Observer
     public function captureLooid($observer)
     {
         $isEnabled = Mage::getStoreConfig('shipping/looship_admin/enabled');
-        if ($isEnabled) {
-            $request = $observer->getEvent()->getControllerAction()->getRequest();
-            $looid = $request->getParam('looid');
-            if ($looid) {
-                Mage::getSingleton('core/session')->setLooid($looid);
-            }
-        }
+        if (!$isEnabled) return;
+        $request = $observer->getEvent()->getControllerAction()->getRequest();
+        $looid = $request->getParam('looid');
+        if (!$looid || $looid == '') return;
+
+        $reference = Mage::app()->getRequest()->getHeader('referer') || Mage::app()->getRequest()->getServer('HTTP_REFERER');
+        $media = array(
+            "reference"    => $reference,
+            "utm_source"   => $request->getParam('utm_source'),
+            "utm_campaign" => $request->getParam('utm_campaign'),
+            "utm_medium"   => $request->getParam('utm_medium'),
+        );
+        Mage::getSingleton('core/session')->setLooId($looid);
+        Mage::getSingleton('core/session')->setLooMedia($media);
     }
 
-    public function quoteShipping(Varien_Event_Observer $observer)
+    public function orderConfirmed(Varien_Event_Observer $observer)
+    {
+        $order = $observer->getEvent()->getOrder();
+        if ($order->getStatus() == Mage_Sales_Model_Order::STATE_COMPLETE) return;
+        
+        $isEnabled = Mage::getStoreConfig('shipping/looship_admin/enabled');
+        if (!$isEnabled) return;
+
+        $orderNumber = $order->getIncrementId();
+        $job_id = Mage::getSingleton('core/session')->getLooJobid();
+        $url = Mage::helper('looship_admin')->getHost() . '/v3/order/' . $orderNumber . '/confirmed';
+        $postData = array(
+            "job_id" => $job_id,
+        );                
+        $newData = Mage::helper('looship_admin')->postRequest($url, $postData);                
+    }
+
+    public function shippingQuote(Varien_Event_Observer $observer)
     {
         $isEnabled = Mage::getStoreConfig('shipping/looship_admin/enabled');
-        if ($isEnabled) 
+        if (!$isEnabled) return; 
+        $voucher = Mage::helper('looship_admin')->getLooId();
+        if ($voucher == '') return;
+
+        $store = Mage::app()->getStore();
+        $shippingOrigin = $store->getConfig('shipping/origin/postcode');
+        $quote = $observer->getQuote();        
+        if (!$quote) return;
+
+        $shippingAddress = $quote->getShippingAddress();
+        $shippingRates = $shippingAddress->getAllShippingRates();
+        $freights = Mage::helper('looship_admin')->getShippingRates($shippingRates);
+        if (count($freights) <= 0) return;
+
+        $items = Mage::helper('looship_admin')->getOrderItems();                        
+        if (count($items) <= 0) return;
+    
+        $checkoutSession = Mage::getSingleton('checkout/session');
+        $simulate = $checkoutSession->getQuote() && $checkoutSession->getLastRealOrderId();
+        $media = Mage::helper('looship_admin')->getLooMedia();
+        $destinationZipCode = $shippingAddress->getPostcode();
+
+        $postData = array(
+            "simulate"  => $simulate,
+            "media"     => $media,
+            "voucher"   => $voucher,
+            "warehouse" => array(
+                "zipcode" => $shippingOrigin,
+            ),
+            "shipping"  => array(
+                "zipcode" => $destinationZipCode,
+            ),
+            "items"     => $items,
+            "freights"  => $freights,
+        );
+
+        $url = Mage::helper('looship_admin')->getHost() . '/v3/voucher/validate';
+        $newData = Mage::helper('looship_admin')->postRequest($url, $postData);
+        if (isset($newData) && $newData['valid'] == true)
         {
-            $voucher = Mage::helper('looship_admin')->getLooid();
-
-            if ($voucher !== '') 
+            $newFreights = $newData['freights'];
+            if (isset($newFreights) && is_array($newFreights))
             {
-                $store = Mage::app()->getStore();
-                $shippingOrigin = $store->getConfig('shipping/origin/postcode');
-                $quote = $observer->getQuote();        
-                $shippingRates = [];
-                $destinationZipCode = null;
-                if ($quote !== null) {
-                    $shippingAddress = $quote->getShippingAddress();
-                    $destinationZipCode = $shippingAddress->getPostcode();
-                    $shippingRates = $shippingAddress->getAllShippingRates();
-                }
-
-                $items = [];
-                $cart = Mage::getModel('checkout/cart');
-                $cart_items = $cart->getQuote()->getAllItems();
-                foreach ($cart_items as $item) 
+                Mage::getSingleton('core/session')->setLooJobid($newData['job_id']);
+                foreach ($shippingAddress->getShippingRatesCollection() as $shippingRate) 
                 {
-                    $productId = $item->getProductId();
-                    $product = Mage::getModel('catalog/product')->load($productId);
-                    $items[] = array(
-                        "sku"      => $product->getSku(),
-                        "name"     => $product->getName(),
-                        "amount"   => $item->getQty(),
-                        "subtotal" => $item->getPrice(),
-                        "taxonomy" => $product->getAttributeText('taxonomy'),
-                        "weight"   => (float) $product->getWeight(),
-                    );
-                }
-
-                $freights = [];
-                foreach ($shippingRates as $rate) 
-                {
-                    if ($rate !== null) {
-                        $freights[] = array(
-                            "label" => $rate->getCode(),
-                            "time"  => (int) $rate->getTimeInTransit(),
-                            "value" => $rate->getPrice(),
-                        );
-                    }
-                }
-
-                $postData = array(
-                    "simulate" => false,
-                    "media"    => array(
-                        "reference"    => "site",
-                        "utm_source"   => "google",
-                        "utm_campaign" => "musical",
-                        "utm_medium"   => "site"
-                    ),
-                    "voucher"  => $voucher,
-                    "warehouse" => array(
-                        "zipcode" => $shippingOrigin,
-                    ),
-                    "shipping" => array(
-                        "zipcode" => $destinationZipCode,
-                    ),
-                    "items"    => $items,
-                    "freights" => $freights,
-                );
-
-                $jsonData = json_encode($postData);
-
-                $client = new Varien_Http_Client();
-                $url = 'https://api.looship.com.br/v3/voucher/validate';
-                $token = Mage::getStoreConfig('shipping/looship_admin/token');
-                $headers = array(
-                    'Authorization' => 'Bearer ' . $token,
-                    'Content-Length' => strlen($jsonData),
-                    'Content-Type' => 'application/json',
-                );
-                try {
-                    $client->setHeaders($headers);
-                    $client->setUri($url)
-                        ->setConfig(array('maxredirects' => 0, 'timeout' => 300))
-                        ->setMethod(Zend_Http_Client::POST)
-                        ->setRawData($jsonData, 'application/json');
-                    $response = $client->request();
-                    Mage::log($jsonData, null, 'looship.log');
-                    if ($response->isSuccessful())
+                    $targetShippingRateLabel = $shippingRate->getCode(); 
+                    foreach ($newFreights as $freight) 
                     {
-                        $body = $response->getBody();
-                        $responseData = json_decode($body, true);
-                        $newData = $responseData['data'];
-                        if ($newData['valid'] == true)
+                        if ($freight['label'] == $targetShippingRateLabel) 
                         {
-                            Mage::getSingleton('core/session')->setJobid($newData['job_id']);
-                            $newFreights = $newData['freights'];
-                            if (isset($newFreights) && is_array($newFreights))
-                            {
-                                foreach ($shippingRates as $shippingRate) 
-                                {
-                                    $targetShippingRateLabel = $shippingRate->getCode(); 
-                                    foreach ($newFreights as $freight) 
-                                    {
-                                        if ($freight['label'] == $targetShippingRateLabel) 
-                                        {
-                                            $newRatePrice = $freight['value'];
-                                            $shippingRate->setPrice($newRatePrice);
-                                            $shippingRate->save();
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
+                            $newRatePrice = (float) $freight['value'];
+                            $shippingRate->setPrice($newRatePrice);
+                            $shippingRate->save();
+                            break;
                         }
                     }
-                } catch (Exception $e) {
-                    Mage::log(json_encode($e->getMessage()), null, 'request.log');
                 }
             }
-            
         }
     }
 }
+?>
